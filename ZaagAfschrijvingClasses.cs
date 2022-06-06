cf71628fbd266a725ba28e4a6ebfc5c567fb2641
GerstenJoch <sanderr06@gmail.com>
2022-06-06 13:17:42 +0200

    Update

---
+++ ZaagAfschrijvingClasses.cs
@@ -0,0 +1,15 @@
+﻿using LINQtoCSV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boikon
+{
+    public class SQLData
+    {
+
+        public static string connstr = "server=localhost;uid=root;pwd=P@ssW0rd69;database=boikon";
+    }
+}
